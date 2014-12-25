73174b343c6b046d60a2a17b7a1a297edca23f0c
minux <minux.ma@gmail.com>
2014-12-25 03:21:48 -0500

    tests/unsupported_argument_type_to_builtin_function.go: add.
    Signed-off-by: minux <minux.ma@gmail.com>

--- tests/unsupported_argument_type_to_builtin_function.go
+++ tests/unsupported_argument_type_to_builtin_function.go
@@ -1,3 +1,9 @@
 //po:MsgId "unsupported argument type to builtin function"
-//po:MsgStr "%<__builtin_frame_address%>的实参不受支持"
+//po:MsgStr "内建函数 print/println 不支持这种实参类型"
 
+package p
+
+func F() {
+	println(F())
+	print(F())
+}
