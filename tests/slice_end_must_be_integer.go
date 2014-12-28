0d3b39b04d71d33b9906fd0a2696359db969765e
minux <minux.ma@gmail.com>
2014-12-28 04:04:25 -0500

    tests/slice_end_must_be_integer.go: add.
    Signed-off-by: minux <minux.ma@gmail.com>

--- tests/slice_end_must_be_integer.go
+++ tests/slice_end_must_be_integer.go
@@ -1,3 +1,11 @@
 //po:MsgId "slice end must be integer"
-//po:MsgStr "切片结尾必须是整数"
+//po:MsgStr "切片的端点索引必须是整数"
 
+package p
+
+var x []byte
+
+var (
+	//_ = x[2.718:] // index must be integer
+	_ = x[:3.14] // slice end must be integer
+)
