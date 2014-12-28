0a796011aa63143f5182cfaf7c7e19a767dea3a0
minux <minux.ma@gmail.com>
2014-12-28 03:46:20 -0500

    tests/invalid_use_of_calling_non_variadic_function.go: add.
    Signed-off-by: minux <minux.ma@gmail.com>

--- tests/invalid_use_of_calling_non_variadic_function.go
+++ tests/invalid_use_of_calling_non_variadic_function.go
@@ -1,3 +1,10 @@
 //po:MsgId "invalid use of %<...%> calling non-variadic function"
-//po:MsgStr "在非成员函数中使用%<this%>无效"
+//po:MsgStr "使用<%...%>调用非变参函数"
 
+package p
+
+func F([]int) int
+
+var x []int
+
+var y = F(x...)
--- tests/invalid_use_of_with_non_slice.go
+++ tests/invalid_use_of_with_non_slice.go
@@ -1,3 +1,9 @@
 //po:MsgId "invalid use of %<...%> with non-slice"
 //po:MsgStr "在文件作用域使用%<this%>无效"
 
+package p
+
+func F(int, []int) int
+
+var x [1]int
+var y = F(1, x...)
