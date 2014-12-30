84c09f54ea3dca4c240175f2b15c0e7b762975ff
minux <minux.ma@gmail.com>
2014-12-30 02:19:41 -0500

    tests/impossible_type_assertion_type_does_not_implement_interface.go: add.
    Signed-off-by: minux <minux.ma@gmail.com>

--- tests/impossible_type_assertion_type_does_not_implement_interface.go
+++ tests/impossible_type_assertion_type_does_not_implement_interface.go
@@ -1,3 +1,16 @@
 //po:MsgId "impossible type assertion: type does not implement interface"
-//po:MsgStr ""
+//po:MsgStr "不可能的类型断言：所断言的类型并不实现该接口"
 
+package p
+
+type I interface {
+	F()
+}
+
+type T1 struct{}
+
+func (*T1) F() {}
+
+var x I
+
+var y = x.(T1)
