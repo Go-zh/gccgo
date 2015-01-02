1b1c5e7d509776a52a169a53a9879179ef2b6ac7
minux <minux.ma@gmail.com>
2015-01-02 00:25:17 -0500

    tests/parentheses_required_around_this_composite_literal_to_avoid_parsing_ambiguity.go: add.
    Signed-off-by: minux <minux.ma@gmail.com>

--- tests/parentheses_required_around_this_composite_literal_to_avoid_parsing_ambiguity.go
+++ tests/parentheses_required_around_this_composite_literal_to_avoid_parsing_ambiguity.go
@@ -1,3 +1,12 @@
 //po:MsgId "parentheses required around this composite literal to avoid parsing ambiguity"
-//po:MsgStr ""
+//po:MsgStr "为避免有歧义的语法分析，必须在用圆括号将这个复合字面括起来"
 
+package p
+
+type T struct{}
+
+func F() {
+	if &T{} == nil {
+		return
+	}
+}
