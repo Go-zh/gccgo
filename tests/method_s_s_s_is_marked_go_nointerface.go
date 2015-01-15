8fec499084fc0a551cc47794c883b5018e336d1d
Minux <minux@golang.org>
2015-01-15 03:44:31 -0500

    tests: 为 gccgo.pot 倒数四条构造示例并初步翻译
    Change-Id: I2975f7132d83793f66f6906de173e62e0b505322
    Signed-off-by: Minux <minux@golang.org>

--- tests/method_s_s_s_is_marked_go_nointerface.go
+++ tests/method_s_s_s_is_marked_go_nointerface.go
@@ -1,3 +1,15 @@
 //po:MsgId "method %s%s%s is marked go:nointerface"
-//po:MsgStr "方法%s%s%s需要一个指针"
+//po:MsgStr "方法 %s%s%s 被标记为 go:nointerface"
 
+package p
+
+type I interface {
+	F()
+}
+
+type x struct{}
+
+//go:nointerface
+func (x) F() {}
+
+var _ I = x{}
